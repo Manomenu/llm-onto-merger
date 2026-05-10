8f94b6eafa76872418d33908be0fa025873b9874
Manomenu <gugnowskiofficial@gmail.com>
2026-05-10 21:09:16 +0200

    progress

--- src/llm_onto_merger/merge_environments/module.py
+++ src/llm_onto_merger/merge_environments/module.py
@@ -1,5 +1,4 @@
-from typing import List
-
+from pydantic import BaseModel
 from rdflib import Graph
 
 from llm_onto_merger.extract_environments.merge_environment import MergeEnvironment
@@ -8,9 +7,18 @@ from llm_onto_merger.ontology import Entity, entities_to_graph
 from .agent import merge_agent
 
 
+class MergedOntology(BaseModel):
+    Merged_Ontology: list[Entity]
+
+
+_MERGED_ONTOLOGY_SCHEMA = MergedOntology.model_json_schema()
+
+
 class MergeEnvironmentsModule:
     async def merge(self, merge_environment: MergeEnvironment) -> Graph:
         response = await merge_agent.run(
-            merge_environment.to_string(), options={"response_format": List[Entity]}
+            merge_environment.to_string(),
+            options={"response_format": _MERGED_ONTOLOGY_SCHEMA},
         )
-        return entities_to_graph(response.value)
+        merged = MergedOntology.model_validate(response.value)
+        return entities_to_graph(merged.Merged_Ontology)
